05cf0be1427b7fa91a76aed177da55e3d5a1600b
Emin Yildiz <eminyildiz122@gmail.com>
2023-07-24 17:11:52 +0300

    add ehcache

---
+++ ehcache/src/main/java/com/example/ehcache/config/CacheConfigV2.java
@@ -0,0 +1,11 @@
+package com.example.ehcache.config;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+//@CacheConfig
+public class CacheConfigV2 {
+}
---
+++ ehcache/src/test/java/com/example/ehcache/EhcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ehcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
